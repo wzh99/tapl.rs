878e48b9b900f1ca6cb6fea384b2c984626469de
wzh99 <wzh1999_frog@126.com>
2020-01-31 15:00:56 +0800

    Complete all evaluation rules.

--- src/full_simple.rs
+++ src/full_simple.rs
@@ -67,61 +67,55 @@ impl Term {
     }
 }
 
-trait VarMap {
-    fn on_var(&self, t: &Rc<Term>, c: isize) -> Rc<Term>;
+fn map_var<F>(f: &F, t: &Rc<Term>) -> Rc<Term> where
+    F: Fn(&Rc<Term>, isize) -> Rc<Term> {
+    walk(f, t, 0)
 }
 
-fn map_var(on_var: &dyn VarMap, t: &Rc<Term>) -> Rc<Term> {
-    walk(on_var, t, 0)
-}
-
-fn walk(map: &dyn VarMap, t: &Rc<Term>, c: isize) -> Rc<Term> {
+fn walk<F>(f: &F, t: &Rc<Term>, c: isize) -> Rc<Term> where
+    F: Fn(&Rc<Term>, isize) -> Rc<Term> {
     match t.as_ref() {
-        Term::Var(_) => map.on_var(t, c),
+        Term::Var(_) => f(t, c),
         Term::Abs(param_type, t1) =>
-            rc(Term::Abs(param_type.clone(), walk(map, t1, c + 1))),
+            rc(Term::Abs(param_type.clone(), walk(f, t1, c + 1))),
         Term::App(t1, t2) =>
-            rc(Term::App(walk(map, t1, c), walk(map, t2, c))),
-        Term::IsZero(t1) => rc(Term::IsZero(walk(map, t1, c))),
-        Term::Pred(t1) => rc(Term::Pred(walk(map, t1, c))),
-        Term::Succ(t1) => rc(Term::Succ(walk(map, t1, c))),
+            rc(Term::App(walk(f, t1, c), walk(f, t2, c))),
+        Term::IsZero(t1) => rc(Term::IsZero(walk(f, t1, c))),
+        Term::Pred(t1) => rc(Term::Pred(walk(f, t1, c))),
+        Term::Succ(t1) => rc(Term::Succ(walk(f, t1, c))),
         Term::If(t1, t2, t3) =>
-            rc(Term::If(walk(map, t1, c), walk(map, t2, c), walk(map, t3, c))),
+            rc(Term::If(walk(f, t1, c), walk(f, t2, c), walk(f, t3, c))),
         Term::Ascribe(t1, type_t1) =>
-            rc(Term::Ascribe(walk(map, t1, c), type_t1.clone())),
+            rc(Term::Ascribe(walk(f, t1, c), type_t1.clone())),
         Term::Let(t1, t2) =>
-            rc(Term::Let(walk(map, t1, c), walk(map, t2, c + 1))),
+            rc(Term::Let(walk(f, t1, c), walk(f, t2, c + 1))),
         Term::Record(fields) =>
             rc(Term::Record(fields.iter()
-                .map(|(l, ti)| (l.clone(), walk(map, ti, c)))
+                .map(|(l, ti)| (l.clone(), walk(f, ti, c)))
                 .collect()
             )),
         Term::Proj(t1, l) =>
-            rc(Term::Proj(walk(map, t1, c), l.clone())),
+            rc(Term::Proj(walk(f, t1, c), l.clone())),
         Term::Tag(l, t1, type_t1) =>
-            rc(Term::Tag(l.clone(), walk(map, t1, c), type_t1.clone())),
+            rc(Term::Tag(l.clone(), walk(f, t1, c), type_t1.clone())),
         Term::Case(t1, cases) =>
-            rc(Term::Case(walk(map, t1, c), cases.iter()
-                .map(|(l, ti)| (l.clone(), walk(map, ti, c + 1)))
+            rc(Term::Case(walk(f, t1, c), cases.iter()
+                .map(|(l, ti)| (l.clone(), walk(f, ti, c + 1)))
                 .collect()
             )),
-        Term::Fix(t1) => rc(Term::Fix(walk(map, t1, c))),
+        Term::Fix(t1) => rc(Term::Fix(walk(f, t1, c))),
         _ => t.clone()
     }
 }
 
 fn shift(d: isize, t: &Rc<Term>) -> Rc<Term> {
-    struct Shift { d: isize }
-    impl VarMap for Shift {
-        fn on_var(&self, t: &Rc<Term>, c: isize) -> Rc<Term> {
-            if let Term::Var(x) = t.as_ref() {
-                if *x >= c { rc(Term::Var(*x + self.d)) } else { t.clone() }
-            } else {
-                panic!()
-            }
+    map_var(&|t, c| {
+        if let Term::Var(x) = t.as_ref() {
+            if *x >= c { rc(Term::Var(*x + d)) } else { t.clone() }
+        } else {
+            panic!()
         }
-    }
-    map_var(&Shift{d}, t)
+    }, t)
 }
 
 fn subst_top(s: &Rc<Term>, t: &Rc<Term>) -> Rc<Term> {
@@ -129,17 +123,13 @@ fn subst_top(s: &Rc<Term>, t: &Rc<Term>) -> Rc<Term> {
 }
 
 fn subst(j: isize, s: &Rc<Term>, t: &Rc<Term>) -> Rc<Term> {
-    struct Subst { j: isize, s: Rc<Term> }
-    impl VarMap for Subst {
-        fn on_var(&self, t: &Rc<Term>, c: isize) -> Rc<Term> {
-            if let Term::Var(x) = t.as_ref() {
-                if *x == self.j + c { shift(c, &self.s) } else { t.clone() }
-            } else {
-                panic!()
-            }
+    map_var(&|t, c| {
+        if let Term::Var(x) = t.as_ref() {
+            if *x == j + c { shift(c, s) } else { t.clone() }
+        } else {
+            panic!()
         }
-    }
-    map_var(&Subst{j, s: s.clone()}, t)
+    }, t)
 }
 
 pub fn eval(t: &Rc<Term>) -> Rc<Term> {
@@ -151,6 +141,7 @@ pub fn eval(t: &Rc<Term>) -> Rc<Term> {
 
 fn eval_once(t: &Rc<Term>) -> Option<Rc<Term>> {
     match t.as_ref() {
+        // Lambda calculus
         Term::App(v1, v2) if v1.is_val() && v2.is_val() =>
             if let Term::Abs(_, t12) = v1.as_ref() {
                 Some(subst_top(v2, t12))
@@ -159,12 +150,7 @@ fn eval_once(t: &Rc<Term>) -> Option<Rc<Term>> {
             Some(rc(Term::App(v1.clone(), eval_once(t2)?))),
         Term::App(t1, t2) =>
             Some(rc(Term::App(eval_once(t1)?, t2.clone()))),
-        Term::If(t1, t2, t3) =>
-            match t1.as_ref() {
-                Term::True => Some(t2.clone()),
-                Term::False => Some(t3.clone()),
-                _ => Some(rc(Term::If(eval_once(t1)?, t2.clone(), t3.clone())))
-            }
+        // Nat
         Term::Succ(t1) => Some(rc(Term::Succ(eval_once(t1)?))),
         Term::Pred(t1) =>
             match t1.as_ref() {
@@ -178,6 +164,49 @@ fn eval_once(t: &Rc<Term>) -> Option<Rc<Term>> {
                 Term::Succ(nv) if nv.is_numeric() => Some(rc(Term::False)),
                 _ => Some(rc(Term::IsZero(eval_once(t1)?)))
             }
+        // Conditional
+        Term::If(t1, t2, t3) =>
+            match t1.as_ref() {
+                Term::True => Some(t2.clone()),
+                Term::False => Some(t3.clone()),
+                _ => Some(rc(Term::If(eval_once(t1)?, t2.clone(), t3.clone())))
+            }
+        // Ascription
+        Term::Ascribe(v1, _) if v1.is_val() => Some(v1.clone()),
+        Term::Ascribe(t1, type_t1) =>
+            Some(rc(Term::Ascribe(eval_once(t1)?, type_t1.clone()))),
+        // Let Binding
+        Term::Let(v1, t2) if v1.is_val() => Some(subst_top(v1, t2)),
+        Term::Let(t1, t2) =>
+            Some(rc(Term::Let(eval_once(t1)?, t2.clone()))),
+        // Record
+        Term::Record(fields) =>
+            Some(rc(Term::Record(
+                map_any(fields, |(l, t)| {
+                    if !t.is_val() { Some((l.clone(), eval_once(t)?)) } else { None }
+                })?
+            ))),
+        Term::Proj(v1, l) if v1.is_val() =>
+            if let Term::Record(fields) = v1.as_ref() {
+                assoc(fields, l).map(|ti| ti.clone())
+            } else { None }
+        Term::Proj(t1, l) =>
+            Some(rc(Term::Proj(eval_once(t1)?, l.clone()))),
+        // Variant
+        Term::Tag(l, t1, type_t) =>
+            Some(rc(Term::Tag(l.clone(), eval_once(t1)?, type_t.clone()))),
+        Term::Case(v1, cases) if v1.is_val() =>
+            if let Term::Tag(l, v11, _) = v1.as_ref() {
+                assoc(cases, l).map(|ti| subst_top(v11, ti))
+            } else { None }
+        Term::Case(t1, cases) =>
+            Some(rc(Term::Case(eval_once(t1)?, cases.clone()))),
+        // General Recursion
+        Term::Fix(v1) if v1.is_val() =>
+            if let Term::Abs(_, t12) = v1.as_ref() {
+                Some(subst_top(t, t12))
+            } else { None }
+        Term::Fix(t1) => Some(rc(Term::Fix(eval_once(t1)?))),
         _ => None
     }
 }
--- src/util.rs
+++ src/util.rs
@@ -6,12 +6,33 @@ pub fn rc<T>(term: T) -> Rc<T> {
     Rc::new(term)
 }
 
-/// In discipline, no mutable variables should appear in a functional programming paradigm.
-/// However, the insert method on Vec<T> only accept mutable references. So this function is
-/// used to hide this mutable variable and make the whole program looks functional in general.
+/// Put an element to the front of a list.
 #[inline]
-pub fn cons<T: Clone>(elem: T, v: &Vec<T>) -> Vec<T> {
+pub fn cons<T>(elem: T, v: &Vec<T>) -> Vec<T> where T: Clone {
     let mut new_vec = (*v).clone();
     new_vec.insert(0, elem);
     new_vec
+}
+
+/// Possibly map elements in a `Vec` to another. Return None if none of the elements are
+/// actually mapped
+#[inline]
+pub fn map_any<T, F>(v: &Vec<T>, mut f: F) -> Option<Vec<T>> where
+    T: Clone, F: FnMut(&T) -> Option<T> {
+    let mut mapped = false;
+    let new_map = v.iter()
+        .map(|elem| {
+            match f(elem) {
+                Some(r) => { mapped = true; r }
+                None => elem.clone()
+            }
+        }).collect();
+    if mapped { Some(new_map) } else { None }
+}
+
+/// Find associated value of a given key in a `Vec<K, V>`
+#[inline]
+pub fn assoc<'a, K, V>(vec: &'a Vec<(K, V)>, key: &K) -> Option<&'a V> where
+    K: PartialEq {
+    vec.iter().find(|(k, _)| k == key).map(|(_, v) | v)
 }
